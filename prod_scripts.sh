97a2e769bde334fc019a362d80de1499eff7369f
Divya Bhargavi Yakkala <divya.yakkala@valuelabs.com>
2020-12-04 21:50:46 +0530

    Updated commands to copy and remove

--- prod_scripts.sh
+++ prod_scripts.sh
@@ -1,9 +1,6 @@
-echo ${TRAVIS_BRANCH}
-echo $TRAVIS_BRANCH
-find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
+# Production script
 sed "/Version/s/>[^<]*</>$RELEASE_VERSION</" manifest.xml
 sed -i.bak "s|$DEFAULT_LOCALHOST|$PROD_URL|g" manifest.xml
-echo "PRINTING MANIFEST AT MASTER"
-cat manifest.xml
 mkdir $RELEASE_VERSION
 cp -r `ls -A | grep -v "$RELEASE_VERSION"` ./$RELEASE_VERSION
+rm -rf !($RELEASE_VERSION)
